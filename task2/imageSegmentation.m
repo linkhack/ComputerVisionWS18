377eab897e9a0291e1e77ee5655c85005aa51703
patricklink314 <patricklink314@gmail.com>
2018-11-11 15:27:52 +0100

    Update task2/imageSegmentation.m

--- task2/imageSegmentation.m
+++ task2/imageSegmentation.m
@@ -1,4 +1,4 @@
-image = imread('./Images/Papyrustext.jpg');
+image = imread('./Images/mm.jpg');
 %image = imresize(image,0.5);
 dimensions = size(image);
 if(size(dimensions,2)==2)
@@ -8,10 +8,10 @@ image = im2double(image);
 [X,Y] = meshgrid(1:dimensions(2),1:dimensions(1));
 X=X/dimensions(2);
 Y=Y/dimensions(1);
-coordinates = cat(3,Y,X);
-%image = cat(3,image,coordinates);
+%coordinates = cat(3,Y,X);
+image = cat(3,image,coordinates);
 imageVector = reshape(image,[],size(image,3));
-[centers, indicatorMatrix] = myKMeans(imageVector,5,0.001);
+[centers, indicatorMatrix] = myKMeans(imageVector,55,0.001);
 clusterImage = indicatorMatrix*centers;
 imageReshape = reshape(imageVector(:,1:dimensions(3)),dimensions);
 clusterImage = reshape(clusterImage(:,1:dimensions(3)),dimensions);
