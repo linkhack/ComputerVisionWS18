925b354d117819b8e32ade14ea16a5d9d40183ee
patricklink314 <patricklink314@gmail.com>
2018-11-21 21:28:26 +0100

    double quotes

--- Task1/report_figures.m
+++ Task1/report_figures.m
@@ -8,23 +8,23 @@ figure()
 image1 = image_segmentation('./res/future.jpg', K, precision, false, false);
 subplot(2,2,1)
 imshow(image1)
-title("K=3, no coordinates used")
+title('K=3, no coordinates used')
 
 image2 = image_segmentation('./res/future.jpg', K, precision, true, false);
 subplot(2,2,2)
 imshow(image2)
-title("K=3, coordinates used")
+title('K=3, coordinates used')
 
 K=5;
 image1 = image_segmentation('./res/future.jpg', K, precision, false, false);
 subplot(2,2,3)
 imshow(image1)
-title("K=5, no coordinates used")
+title('K=5, no coordinates used')
 
 image2 = image_segmentation('./res/future.jpg', K, precision, true, false);
 subplot(2,2,4)
 imshow(image2)
-title("K=5, coordinates used")
+title('K=5, coordinates used')
 
 %%
 K=3;
@@ -32,24 +32,24 @@ figure()
 image1 = image_segmentation('./res/mm.jpg', K, precision, false, false);
 subplot(2,2,1)
 imshow(image1)
-title("K=3, no coordinates used")
+title('K=3, no coordinates used')
 
 image2 = image_segmentation('./res/mm.jpg', K, precision, true, false);
 subplot(2,2,2)
 imshow(image2)
-title("K=3, coordinates used")
+title('K=3, coordinates used')
 
 
 K=5;
 image1 = image_segmentation('./res/mm.jpg', K, precision, false, false);
 subplot(2,2,3)
 imshow(image1)
-title("K=5, no coordinates used")
+title('K=5, no coordinates used')
 
 image2 = image_segmentation('./res/mm.jpg', K, precision, true, false);
 subplot(2,2,4)
 imshow(image2)
-title("K=5, coordinates used")
+title('K=5, coordinates used')
 
 %%
 figure()
@@ -57,23 +57,23 @@ K=3;
 image1 = image_segmentation('./res/simple.PNG', K, precision, false, false);
 subplot(2,2,1)
 imshow(image1)
-title("K=3, no coordinates used")
+title('K=3, no coordinates used')
 
 image2 = image_segmentation('./res/simple.PNG', K, precision, true, false);
 subplot(2,2,2)
 imshow(image2)
-title("K=3, coordinates used")
+title('K=3, coordinates used')
 
 K=5;
 image1 = image_segmentation('./res/simple.PNG', K, precision, false, false);
 subplot(2,2,3)
 imshow(image1)
-title("K=5, no coordinates used")
+title('K=5, no coordinates used')
 
 image2 = image_segmentation('./res/simple.PNG', K, precision, true, false);
 subplot(2,2,4)
 imshow(image2)
-title("K=5, coordinates used")
+title('K=5, coordinates used')
 
 %%
 %%Hard to see stuff, so do K=5 image = mm.jpg with distinct colors
@@ -82,12 +82,12 @@ K=3;
 image1 = image_segmentation('./res/mm.jpg', K, precision, false, true);
 subplot(1,2,1)
 imshow(image1)
-title("K=5, no coordinates used")
+title('K=5, no coordinates used')
 
 image2 = image_segmentation('./res/mm.jpg', K, precision, true, true);
 subplot(1,2,2)
 imshow(image2)
-title("K=5, coordinates used")
+title('K=5, coordinates used')
 
 %%
 % Change number of clusters. As we don't see results very good. Use
@@ -101,7 +101,7 @@ for i = 1:6
     seg_image = image_segmentation('./res/mm.jpg',Karray(i),precision,false,true);
     subplot(3,2,i)
     imshow(seg_image)
-    title("K= " + Karray(i) + ", no coordinates used")
+    title('K= ' + Karray(i) + ', no coordinates used')
 end
 %%
 figure()
@@ -109,7 +109,7 @@ for i = 1:6
     seg_image = image_segmentation('./res/mm.jpg',Karray(i),precision,true,true);
     subplot(3,2,i)
     imshow(seg_image)
-    title("K= " + Karray(i) + ", coordinates used")
+    title('K= ' + Karray(i) + ', coordinates used')
 end
 
 %%
@@ -118,6 +118,6 @@ for i=1:20
     figure()
     seg_image = image_segmentation('./res/simple.PNG',3,precision,false,true);
     imshow(seg_image)
-    title("Bad initialization")
+    title('Bad initialization')
 end
 
