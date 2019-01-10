04c85679adc2c9b5301a82418a3b7ca6a2c646d6
patricklink314 <patricklink314@gmail.com>
2019-01-10 15:16:50 +0100

    Task 2 start
    Building Vocabulary works

---
+++ task2/bow_patrick/BuildVocabulary.m
@@ -0,0 +1,27 @@
+function vocabulary = BuildVocabulary(folder,num_clusters)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+vocabulary = zeros([128,num_clusters])
+
+folders = dir(folder);
+sift_features = [];
+
+%% Extract Sift Features
+for i = 3:length(folders)
+    current_folder = strcat(folders(i).folder,'\',folders(i).name);
+    train_image_folder = dir(current_folder);
+    for image_index = 3:length(train_image_folder)
+        image_path = strcat(train_image_folder(image_index).folder,'\',train_image_folder(image_index).name);
+        img = im2single(imread(image_path));
+        pixels = numel(img);
+        step = floor(sqrt(pixels/100)) % area/(step^2) = number of descriptors
+        [f, d] = vl_dsift(img,'step',step,'fast','fast');
+        sift_features = [sift_features, d];
+    end
+end
+
+%% Calculate Vocabulary by k-means
+vocabulary = vl_kmeans(double(sift_features), num_clusters);
+
+end
+
